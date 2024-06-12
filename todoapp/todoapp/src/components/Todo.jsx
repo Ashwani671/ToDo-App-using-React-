1b9c9e2e0c9efc309eafc7db3de3f12de7d47d9f
Ashwani Gupta <131233369+Ashwani671@users.noreply.github.com>
2024-06-12 11:27:26 +0530

    Add files via upload

---
+++ todoapp/todoapp/src/components/Todo.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+import Form from "./Form";
+import TodoList from "./TodoList";
+import Footer from "./Footer";
+
+export default function Todo() {
+  const [todos, setTodos] = useState([]);
+  const completedTodos = todos.filter((todo) => todo.done).length;
+  const totalTodos = todos.length;
+  return (
+    <div>
+      <Form todos={todos} setTodos={setTodos} />
+      {/* {console.log(todos)} */}
+      {/* {todos.map((item) => (
+        <h3>{item}</h3>
+      ))} */}
+      <TodoList todos={todos} setTodos={setTodos} />
+      {/* <h1>{completedTodos}</h1> */}
+      <Footer completedTodos={completedTodos} totalTodos={totalTodos} />
+    </div>
+  );
+}
---
+++ todoapp/todoapp/src/components/Todoitem.jsx
@@ -0,0 +1,38 @@
+import styles from "./todoitem.module.css";
+
+export default function Todoitem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    console.log("Delete clicked for item", item);
+    setTodos(todos.filter((todo) => todo !== item));
+  }
+
+  function handleClick(name) {
+    // console.log("item clicked", name);
+    const newArray = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newArray);
+    // console.log(todos);
+  }
+  const className = item.done ? styles.completed : "";
+
+  return (
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        <span className={className} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deletebutton}
+          >
+            x
+          </button>
+        </span>
+      </div>
+
+      <hr className={styles.line} />
+    </div>
+  );
+}
